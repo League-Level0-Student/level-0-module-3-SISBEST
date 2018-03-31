b2d50020c1f4173d595fd69b6b2291d3cfb2f288
NickSanford4 <league@iMac.attlocal.net>
2018-03-31 16:48:15 -0700

    ObedientRobot (YAY! DONE WITH MODULE!)

--- src/extras/ObedientRobot.java
+++ src/extras/ObedientRobot.java
@@ -10,14 +10,23 @@ public class ObedientRobot {
 		// TODO Auto-generated method stub
 		Robot obedientRobot = new Robot();
 		String Shape = JOptionPane.showInputDialog(null, "Square, Circle, or Triangle?");
+		String Color = JOptionPane.showInputDialog(null, "What color?");
+		if (Color.equals("Red"))
+			obedientRobot.setPenColor(255,0,0);
+		if (Color.equals("Green"))
+			obedientRobot.setPenColor(0,255,0);
+		if (Color.equals("Blue"))
+			obedientRobot.setPenColor(0,0,255);
+
+
 		if (Shape.equals("Square")) {
-			drawSquare();
+			drawSquare(obedientRobot);
 		}
 		else if (Shape.equals("Triangle")) {
-			drawSquare();
+			drawTriangle(obedientRobot);
 		}
 		else if (Shape.equals("Circle")) {
-			drawCircle();
+			drawCircle(obedientRobot);
 		}
 
 	}
@@ -25,22 +34,34 @@ public class ObedientRobot {
 	static void drawSquare(Robot obedientRobot) {
 		obedientRobot.penDown();
 		obedientRobot.setSpeed(999);
+		for (int sq = 0; sq<4; sq++) {
 		obedientRobot.move(100);
 		obedientRobot.turn(90);
-		obedientRobot.move(100);
-		obedientRobot.turn(90);
-		obedientRobot.move(100);
-		obedientRobot.turn(90);
-		obedientRobot.move(100);
+		}
+		obedientRobot.hide();
+		
 
 	}
 
-	static void drawTriangle() {
-
+	static void drawTriangle(Robot obedientRobot) {
+		obedientRobot.penDown();
+		obedientRobot.setSpeed(999);	
+		for (int tri = 0; tri<3;tri++) {
+		obedientRobot.turn(120);
+		obedientRobot.move(25);
+		obedientRobot.hide();
+		}
 	}
 
-	static void drawCircle() {
-
+	static void drawCircle(Robot obedientRobot) {
+		obedientRobot.penDown();
+		obedientRobot.setSpeed(999);
+		for (int cir = 0; cir<360;cir++) {
+			obedientRobot.move(1);
+			obedientRobot.turn(1);
+		}
+			
+		}
 	}
 
-}
+
